7669bc6870ca9baf5f9de41d90ea96a935fafee2
Joshua Boothby <Mona.Lisa@example.com>
2025-04-23 20:47:08 -0500

    tree shook

---
+++ src/greetings+.js
@@ -0,0 +1,19 @@
+export function sayHello(name) {
+  return `Hello, ${name}!`;
+}
+
+export function sayGoodbye(name) {
+  return `Goodbye, ${name}!`;
+}
+
+export function sayWelcome(name) {
+  return `Welcome, ${name}!`;
+}
+
+export function sayFarewell(name) {
+  return `Farewell, ${name}!`;
+}
+
+export function sayGoodMorning(name) {
+  return `Good morning, ${name}!`;
+}
